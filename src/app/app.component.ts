2bc326565569e5e44c4794eaf781e317d10e8742
Nawfel Ajari <ajari.nawfel@hotmail.com>
2024-06-22 22:45:21 +0200

    initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,93 +1,13 @@
-import { ApiService } from './services/api.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DialogComponent } from './dialog/dialog.component';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'PlayersCRUD';
-  displayedColumns: string[] = [
-    'playerName',
-    'category',
-    'position',
-    'date',
-    'price',
-    'comment',
-    'action',
-  ];
-  dataSource!: MatTableDataSource<any>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private dialog: MatDialog, private api: ApiService) {}
-  ngOnInit(): void {
-    this.getAllPlayers();
-  }
-
-  openDialog() {
-    this.dialog
-      .open(DialogComponent, {
-        width: '30%',
-      })
-      .afterClosed()
-      .subscribe((val) => {
-        if (val === 'save') {
-          this.getAllPlayers();
-        }
-      });
-  }
-  getAllPlayers() {
-    this.api.getPlayer().subscribe({
-      next: (res) => {
-        this.dataSource = new MatTableDataSource(res);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      },
-      error: (err) => {
-        alert('Error while fetching the records.');
-      },
-    });
-  }
-
-  editPlayer(row: any) {
-    this.dialog
-      .open(DialogComponent, {
-        width: '30%',
-        data: row,
-      })
-      .afterClosed()
-      .subscribe((val) => {
-        if (val === 'update') {
-          this.getAllPlayers();
-        }
-      });
-  }
-
-  deletePlayer(id: number) {
-    this.api.deletePlayer(id).subscribe({
-      next: (res) => {
-        alert('Player deleted successfully.');
-        this.getAllPlayers();
-      },
-      error: () => {
-        alert('Error while deleting player.');
-      },
-    });
-  }
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+export class AppComponent {
+  title = 'temp-project';
 }
