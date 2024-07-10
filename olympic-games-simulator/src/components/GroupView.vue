22752a9c4676881f04e0eb6f9a2b7edd500eecb0
Nawfel Ajari <ajari.nawfel@hotmail.com>
2024-07-10 17:22:38 +0200

    homepage

---
+++ olympic-games-simulator/src/components/GroupView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="group-view">
+    <h2>{{ groupName }}</h2>
+    <ul>
+      <li v-for="team in teams" :key="team">{{ team }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GroupView",
+  props: {
+    groupName: String,
+    teams: Array,
+  },
+};
+</script>
+
+<style scoped>
+.group-view {
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+}
+.group-view h2 {
+  color: #333;
+}
+.group-view ul {
+  list-style-type: none;
+  padding: 0;
+}
+.group-view li {
+  margin: 5px 0;
+}
+</style>
---
+++ olympic-games-simulator/src/components/GroupsOverview.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="groups-overview">
+    <GroupView
+      v-for="group in groups"
+      :key="group.name"
+      :groupName="group.name"
+      :teams="group.teams"
+    />
+  </div>
+</template>
+
+<script>
+import GroupView from "./GroupView.vue";
+
+export default {
+  name: "GroupsOverview",
+  components: {
+    GroupView,
+  },
+  data() {
+    return {
+      groups: [
+        {
+          name: "Groupe A",
+          teams: ["France", "États-Unis", "Guinée", "Nouvelle-Zélande"],
+        },
+        { name: "Groupe B", teams: ["Argentine", "Maroc", "Irak", "Ukraine"] },
+        {
+          name: "Groupe C",
+          teams: ["Ouzbékistan", "Espagne", "Égypte", "République dominicaine"],
+        },
+        { name: "Groupe D", teams: ["Japon", "Paraguay", "Mali", "Israël"] },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.groups-overview {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
